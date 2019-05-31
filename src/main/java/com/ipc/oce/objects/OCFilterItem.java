782a5e12e322c0ec51f175cee6a1f4b6ff34c4ad
Klimovskikh Vitalii <vitavitall@mail.ru>
2019-05-31 11:23:49 +0000

    не работает отключение условия отбора при работе с регистрами
    git-svn-id: https://svn.ilb.ru/repos/jinterbit/trunk@26 eb287c0d-5295-4a28-8978-44a5bd4c2244

--- src/main/java/com/ipc/oce/objects/OCFilterItem.java
+++ src/main/java/com/ipc/oce/objects/OCFilterItem.java
@@ -1,17 +1,16 @@
 /**
- * 
+ *
  */
 package com.ipc.oce.objects;
-
-import org.jinterop.dcom.common.JIException;
-import org.jinterop.dcom.core.JIVariant;
-import org.jinterop.dcom.impls.automation.IJIDispatch;
 
 import com.ipc.oce.OCObject;
 import com.ipc.oce.OCValueList;
 import com.ipc.oce.OCVariant;
 import com.ipc.oce.metadata.OCTypeDescription;
 import com.ipc.oce.varset.EComparisonType;
+import org.jinterop.dcom.common.JIException;
+import org.jinterop.dcom.core.JIVariant;
+import org.jinterop.dcom.impls.automation.IJIDispatch;
 
 /**
  * Используется для управления параметрами отбора по одному элементу
@@ -20,9 +19,8 @@ import com.ipc.oce.varset.EComparisonType;
  * документа (для журнала документов). Объект используется в качестве элемента
  * коллекции Отбор для установки фильтрации в различных выборках, наборах
  * записей и визуальных представлениях.
- * 
+ *
  * @author Konovalov
- * 
  */
 public class OCFilterItem extends OCObject {
 
@@ -47,16 +45,16 @@ public class OCFilterItem extends OCObject {
 	public OCFilterItem(JIVariant aDispatch) throws JIException {
 		super(aDispatch);
 	}
-	
+
 	/**
-	 * Содержит способ сравнения, используемый при фильтрации по элементу отбора. 
+	 * Содержит способ сравнения, используемый при фильтрации по элементу отбора.
 	 * @return
 	 * @throws JIException
 	 */
 	public EComparisonType getComparisonType() throws JIException {
 		return new EComparisonType(get("ComparisonType"));
 	}
-	
+
 	/**
 	 * Установка способа сравнения, используемый при фильтрации по элементу отбора.
 	 * @param type - способ сравнения.
@@ -65,72 +63,72 @@ public class OCFilterItem extends OCObject {
 	public void setComparisonType(EComparisonType type) throws JIException {
 		put("ComparisonType", new JIVariant(ocObject2Dispatch(type)));
 	}
-	
+
 	/**
 	 * Содержит значение, по которому будет осуществляться
 	 * фильтрация. Примечание: Если ВидСравнения принимает значение ВСписке,
 	 * ВСпискеПоИерархии, НеВСписке или НеВСпискеПоИерархии, то тип значения
 	 * СписокЗначений.
-	 * 
+	 *
 	 * @return OCVariant
 	 * @throws JIException
 	 */
 	public OCVariant getValue() throws JIException {
 		return new OCVariant(get("Value"));
 	}
-	
+
 	/**
 	 * Установка значения, по которому будет осуществляться
 	 * фильтрация. Примечание: Если ВидСравнения принимает значение ВСписке,
 	 * ВСпискеПоИерархии, НеВСписке или НеВСпискеПоИерархии, то тип значения
 	 * СписокЗначений.
-	 * 
+	 *
 	 * @throws JIException
 	 */
 	public void setValue(OCVariant variant) throws JIException {
 		put("Value", variant);
 	}
-	
+
 	public void setValue(OCValueList list) throws JIException {
 		put("Value", new JIVariant(ocObject2Dispatch(list)));
 	}
-	
+
 	/**
-	 * Содержит правое значение сравнения, если в качестве вида сравнения используется интервал. 
+	 * Содержит правое значение сравнения, если в качестве вида сравнения используется интервал.
 	 * @return OCVariant
 	 * @throws JIException
 	 */
 	public OCVariant getValueTo() throws JIException {
 		return new OCVariant(get("ValueTo"));
 	}
-	
+
 	/**
-	 * Устанавливает правое значение сравнения, если в качестве вида сравнения используется интервал. 
+	 * Устанавливает правое значение сравнения, если в качестве вида сравнения используется интервал.
 	 * @param variant
 	 * @throws JIException
 	 */
 	public void setValueTo(OCVariant variant) throws JIException {
 		put("ValueTo", variant);
 	}
-	
+
 	/**
-	 * Содержит левое значение сравнения, если в качестве вида сравнения используется интервал. 
+	 * Содержит левое значение сравнения, если в качестве вида сравнения используется интервал.
 	 * @return
 	 * @throws JIException
 	 */
 	public OCVariant getValueFrom() throws JIException {
 		return new OCVariant(get("ValueFrom"));
 	}
-	
+
 	/**
-	 * Устанавливает левое значение сравнения, если в качестве вида сравнения используется интервал. 
+	 * Устанавливает левое значение сравнения, если в качестве вида сравнения используется интервал.
 	 * @param variant
 	 * @throws JIException
 	 */
 	public void setValueFrom(OCVariant variant) throws JIException {
 		put("ValueFrom", variant);
 	}
-	
+
 	/**
 	 * Содержит имя элемента отбора.
 	 * @return String
@@ -139,33 +137,69 @@ public class OCFilterItem extends OCObject {
 	public String getName() throws JIException {
 		return get("Name").getObjectAsString2();
 	}
-	
+
 	/**
 	 * Содержит признак использования элемента отбора в фильтре. Если свойство
 	 * имеет значение Истина, то будет выполняться фильтрация по данному
 	 * элементу отбора, иначе элемент отбора не будет участвовать в фильтрации.
-	 * 
+	 *
 	 * @return Boolean
 	 * @throws JIException
 	 */
 	public Boolean isUse() throws JIException {
 		return get("Use").getObjectAsBoolean();
 	}
-	
+
 	/**
 	 * Устанавливает признак использования элемента отбора в фильтре. Если
 	 * свойство имеет значение Истина, то будет выполняться фильтрация по
 	 * данному элементу отбора, иначе элемент отбора не будет участвовать в
 	 * фильтрации.
-	 * 
-	 * @param признак
-	 *            использования
+	 *
+	 * @param use признак использования
 	 * @throws JIException
 	 */
 	public void setUse(boolean use) throws JIException {
 		put("Use", new JIVariant(use));
 	}
-	
+
+	/**
+	 * Содержит признак использования элемента отбора в фильтре. Если свойство
+	 * имеет значение Истина, то будет выполняться фильтрация по данному
+	 * элементу отбора, иначе элемент отбора не будет участвовать в фильтрации.
+	 *
+	 * @return Boolean
+	 * @throws JIException
+	 */
+	public Boolean isUsage() throws JIException {
+		return get("Usage").getObjectAsBoolean();
+	}
+
+	/**
+	 * Содержит признак использования элемента отбора в фильтре. Если свойство
+	 * имеет значение Истина, то будет выполняться фильтрация по данному
+	 * элементу отбора, иначе элемент отбора не будет участвовать в фильтрации.
+	 *
+	 * @return Boolean
+	 * @throws JIException
+	 */
+	public Boolean getUsage() throws JIException {
+		return get("Usage").getObjectAsBoolean();
+	}
+
+	/**
+	 * Устанавливает признак использования элемента отбора в фильтре. Если
+	 * свойство имеет значение Истина, то будет выполняться фильтрация по
+	 * данному элементу отбора, иначе элемент отбора не будет участвовать в
+	 * фильтрации.
+	 *
+	 * @param usage признак использования
+	 * @throws JIException
+	 */
+	public void setUsage(boolean usage) throws JIException {
+		put("Usage", new JIVariant(usage));
+	}
+
 	/**
 	 * Пользовательское представление элемента отбора.
 	 * @return
@@ -174,7 +208,7 @@ public class OCFilterItem extends OCObject {
 	public String getPresentation() throws JIException {
 		return get("Presentation").getObjectAsString2();
 	}
-	
+
 	/**
 	 * Пользовательское представление элемента отбора.
 	 * @param presentation
@@ -183,7 +217,7 @@ public class OCFilterItem extends OCObject {
 	public void setPresentation(String presentation) throws JIException {
 		put("Presentation", new JIVariant(presentation));
 	}
-	
+
 	/**
 	 * Путь к данным для элемента отбора. Например: Номенклатура.ЕдиницаИзмерения.Код.
 	 * @return String
@@ -192,7 +226,7 @@ public class OCFilterItem extends OCObject {
 	public String getDataPath() throws JIException {
 		return get("DataPath").getObjectAsString2();
 	}
-	
+
 	/**
 	 * Тип значения для отбора.
 	 * @return
@@ -201,18 +235,16 @@ public class OCFilterItem extends OCObject {
 	public OCTypeDescription getValueType() throws JIException {
 		return new OCTypeDescription(get("ValueType"));
 	}
-	
+
 	/**
 	 * Устанавливает значение отбора и флаг использования. При выполнении
 	 * данного метода вид сравнения устанавливается в значение Равно.
 	 * Примечание: Метод имеет смысл применять, если необходимо установить и
 	 * значение и использование отбора.
-	 * 
-	 * @param variant
-	 *            - Значение сравнения.
-	 * @param use
-	 *            - Признак использования отбора по значению.
-	 * @throws JIException 
+	 *
+	 * @param variant Значение сравнения
+	 * @param use Признак использования отбора по значению
+	 * @throws JIException
 	 */
 	public void set(OCVariant variant, boolean use) throws JIException {
 		callMethod("Set", new Object[]{ocVariant2JI(variant), new JIVariant(use)});
